d1ff8cd4bccc1109be4f4c30e349733c6c6fb6f4
arun benjamin <arunbenjamin@aruns-MacBook-Pro.local>
2015-09-10 18:07:07 +0530

    Initial Commit

---
+++ tamilsms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tamilsms
+//
+//  Created by arun benjamin on 10/09/15.
+//  Copyright (c) 2015 arun benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tamilsms/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tamilsms
+//
+//  Created by arun benjamin on 10/09/15.
+//  Copyright (c) 2015 arun benjamin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
